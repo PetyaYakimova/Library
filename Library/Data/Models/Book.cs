242c9d1631dffb2891d7baf672b7cb8f8ef5f369
PetyaYakimova <petya_yakimova@abv.bg>
2024-01-24 22:09:48 +0200

    Add project files.

---
+++ Library/Data/Models/Book.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Library.Data.Models
+{
+    [Comment("Books for the library")]
+    public class Book
+    {
+        [Comment("Primary key")]
+        [Key]
+        public int Id { get; set; }
+
+        [Comment("Title of the book")]
+        [Required]
+        [MaxLength(50)]
+        public string Title { get; set; } = null!;
+
+        [Comment("Author of the book")]
+        [Required]
+        [MaxLength(50)]
+        public string Author { get; set; } = null!;
+
+        [Comment("Description of the book")]
+        [Required]
+        [MaxLength(5000)]
+        public string Description { get; set; } = null!;
+
+        [Comment("Image URL of the book")]
+        [Required]
+        public string ImageUrl { get; set; } = null!;
+
+        [Comment("Rating of the book")]
+        [Required]
+        public decimal Rating { get; set; }
+
+        [Comment("Category ID of the book")]
+        [Required]
+        public int CategoryId { get; set; }
+
+        [ForeignKey(nameof(CategoryId))]
+        public Category Category { get; set; } = null!;
+
+        public List<IdentityUserBook> UsersBooks { get; set; } = new List<IdentityUserBook>();
+    }
+}
---
+++ Library/Data/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Library.Data.Models
+{
+    [Comment("Categories for the books")]
+    public class Category
+    {
+        [Comment("Primary key")]
+        [Key]
+        public int Id { get; set; }
+
+        [Comment("Name of the category")]
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; } = null!;
+
+        public List<Book> Books { get; set; } = new List<Book>();
+    }
+}
